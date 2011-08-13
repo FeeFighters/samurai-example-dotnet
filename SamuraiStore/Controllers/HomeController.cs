97efcd750b524cd02a7104e6b36b61943dbc570a
blackfoks <mail@blackfoks.com>
2011-08-13 13:52:55 +0600

    Some text changed

--- SamuraiStore/Controllers/HomeController.cs
+++ SamuraiStore/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace SamuraiStore.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.Message = "Welcome to Samurai Store!";
 
             return View();
         }
